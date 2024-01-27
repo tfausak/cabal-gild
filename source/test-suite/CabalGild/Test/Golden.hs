04adaf2f585ae7f6c916bc9158b680b6afab5d38
Taylor Fausak <taylor@fausak.me>
2024-01-27 18:34:21 +0000

    Revert "Where are these carriage returns coming from?"
    This reverts commit f66c7306d27143ae5e04f5f4e28075d2bb4040e6.

--- source/test-suite/CabalGild/Test/Golden.hs
+++ source/test-suite/CabalGild/Test/Golden.hs
@@ -7,8 +7,6 @@ import CabalGild.Prelude
 import qualified Control.Exception as Exception
 import qualified Data.ByteString as BS
 import qualified Data.Map as Map
-import qualified Data.Text as Text
-import qualified Data.Text.Encoding as Encoding
 import System.FilePath ((-<.>), (</>))
 import System.IO (hClose, hFlush)
 import System.IO.Temp (withSystemTempFile)
@@ -122,4 +120,4 @@ files =
       p ("cbits" </> "sub" </> "source3.c") "..."
     ]
   where
-    p x y = (x, Encoding.encodeUtf8 $ Text.pack y)
+    p x y = (x, toUTF8BS y)
