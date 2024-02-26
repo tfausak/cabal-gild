4bf9a45cda141919732d79fbf897d4740cedd409
Taylor Fausak <taylor@fausak.me>
2024-02-26 11:46:25 -0600

    Add conversion functions

--- source/library/CabalGild/Type/Output.hs
+++ source/library/CabalGild/Type/Output.hs
@@ -6,3 +6,10 @@ data Output
   = Stdout
   | File FilePath
   deriving (Eq, Ord, Show)
+
+-- | Converts a string into an output. The string @"-"@ will be converted into
+-- 'Stdout', and any other string will be converted into 'File'.
+fromString :: String -> Output
+fromString s = case s of
+  "-" -> Stdout
+  _ -> File s
